38abe9709003a17ccf3d45a33c60b424bb4e423b
aravdaite <aravdaite@gmail.com>
2020-06-11 22:34:51 +0200

    --fixed another issue with wordlist

--- src/components/WordList.js
+++ src/components/WordList.js
@@ -53,7 +53,7 @@ export class WordList extends Component {
             .map((word, index) => (
                 <li className="WordList__item" key={index}>
                     <Button text={word !== null ? (typeof (word) == "string" ? word.toLowerCase() : (word.amount > 1 ? (word.word + " x" + word.amount) : word.word)) : ''}
-                        type="showMeaning" onClick={() => this.showWordDescription(word.word)} />
+                        type="showMeaning" onClick={() => { typeof (word) == "string" ? this.showWordDescription(word) : this.showWordDescription(word.word) }} />
                 </li>
             )
             )
